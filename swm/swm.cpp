6c599503c3ac49fbb2a37c0b3096f187551ae417
Dave Brown <d.brown@bigdavedev.com>
2018-04-27 10:10:39 +0200

    Convert to C++
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- swm/swm.cpp
+++ swm/swm.cpp
@@ -1,9 +1,16 @@
-#include <stdlib.h>
-
 #include <xcb/xcb.h>
 
-static struct xcb_screen_t* xcb_screen       = NULL;
-static xcb_window_t         xcb_focus_window = 0;
+#include <memory>
+#include <utility>
+
+#include <cstdlib>
+
+#include <signal.h>
+
+namespace
+{
+xcb_screen_t*     xcb_screen       = NULL;
+xcb_window_t      xcb_focus_window = 0;
 
 enum focus_mode
 {
@@ -11,9 +18,9 @@ enum focus_mode
 	FOCUS_MODE_INACTIVE
 };
 
-static struct xcb_connection_t* swm_init(void)
+xcb_connection_t* swm_init(void)
 {
-	struct xcb_connection_t* xcb_connection = xcb_connect(NULL, NULL);
+	xcb_connection_t* xcb_connection = xcb_connect(NULL, NULL);
 	if (xcb_connection_has_error(xcb_connection))
 	{
 		return NULL;
@@ -22,8 +29,8 @@ static struct xcb_connection_t* swm_init(void)
 	xcb_screen = xcb_setup_roots_iterator(xcb_get_setup(xcb_connection)).data;
 	xcb_focus_window = xcb_screen->root;
 
-	int      mask      = XCB_CW_EVENT_MASK;
-	uint32_t values[1] = { XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY };
+	int      const mask      = XCB_CW_EVENT_MASK;
+	uint32_t const values[1] = { XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY };
 	xcb_change_window_attributes_checked(xcb_connection,
 	                                     xcb_screen->root,
 	                                     mask,
@@ -33,16 +40,17 @@ static struct xcb_connection_t* swm_init(void)
 	return xcb_connection;
 }
 
-static void swm_shutdown(struct xcb_connection_t* connection)
+void swm_shutdown(xcb_connection_t* connection)
 {
 	xcb_disconnect(connection);
 }
 
-static void swm_focus_window(struct xcb_connection_t* xcb_connection,
-                             xcb_window_t             window,
-                             enum focus_mode          mode)
+void swm_focus_window(xcb_connection_t* xcb_connection,
+                      xcb_window_t      window,
+                      enum focus_mode   mode)
 {
-	uint32_t values[1] = { (mode == FOCUS_MODE_ACTIVE) ? 0xffffffu : 0xff0000u };
+	uint32_t const values[1]
+	    = { (mode == FOCUS_MODE_ACTIVE) ? 0xffffffu : 0xff0000u };
 	xcb_change_window_attributes(xcb_connection,
 	                             window,
 	                             XCB_CW_BORDER_PIXMAP,
@@ -62,10 +70,9 @@ static void swm_focus_window(struct xcb_connection_t* xcb_connection,
 	}
 }
 
-static void swm_subscribe_window(struct xcb_connection_t* connection,
-                                 xcb_window_t             window)
+void swm_subscribe_window(xcb_connection_t* connection, xcb_window_t window)
 {
-	uint32_t event_mask_values[2]
+	uint32_t const event_mask_values[2]
 	    = { XCB_EVENT_MASK_ENTER_WINDOW, XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY };
 	xcb_change_window_attributes(connection,
 	                             window,
@@ -73,76 +80,74 @@ static void swm_subscribe_window(struct xcb_connection_t* connection,
 	                             event_mask_values);
 
 	/* border width */
-	uint32_t border_values[1] = { 0 };
+	uint32_t const border_values[1] = { 0 };
 	xcb_configure_window(connection,
 	                     window,
 	                     XCB_CONFIG_WINDOW_BORDER_WIDTH,
 	                     border_values);
 }
 
-static void swm_handle_create_notify(struct xcb_connection_t* xcb_connection,
-                                     struct xcb_create_notify_event_t* event)
+void swm_handle_create_notify(xcb_connection_t*    xcb_connection,
+                              xcb_generic_event_t* event)
 {
-	if (!event->override_redirect)
+	auto const ev = reinterpret_cast<xcb_create_notify_event_t*>(event);
+	if (!ev->override_redirect)
 	{
-		swm_subscribe_window(xcb_connection, event->window);
-		swm_focus_window(xcb_connection, event->window, FOCUS_MODE_ACTIVE);
+		swm_subscribe_window(xcb_connection, ev->window);
+		swm_focus_window(xcb_connection, ev->window, FOCUS_MODE_ACTIVE);
 	}
 }
 
-static void swm_handle_destroy_notify(struct xcb_connection_t* xcb_connection,
-                                      struct xcb_destroy_notify_event_t* event)
+void swm_handle_destroy_notify(xcb_connection_t*    xcb_connection,
+                               xcb_generic_event_t* event)
 {
-	xcb_kill_client(xcb_connection, event->window);
+	auto const ev = reinterpret_cast<xcb_destroy_notify_event_t*>(event);
+	xcb_kill_client(xcb_connection, ev->window);
 }
 
-static void swm_handle_map_notify(struct xcb_connection_t*       xcb_connection,
-                                  struct xcb_map_notify_event_t* event)
+void swm_handle_map_notify(xcb_connection_t*    xcb_connection,
+                           xcb_generic_event_t* event)
 {
-	if (!event->override_redirect)
+	auto const ev = reinterpret_cast<xcb_map_notify_event_t*>(event);
+	if (!ev->override_redirect)
 	{
-		xcb_map_window(xcb_connection, event->window);
-		swm_focus_window(xcb_connection, event->window, FOCUS_MODE_ACTIVE);
+		uint32_t const values[2]
+		    = { xcb_screen->width_in_pixels, xcb_screen->height_in_pixels };
+		xcb_configure_window(xcb_connection,
+		                     ev->window,
+		                     XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
+		                     values);
+		xcb_map_window(xcb_connection, ev->window);
+		swm_focus_window(xcb_connection, ev->window, FOCUS_MODE_ACTIVE);
 	}
 }
 
-static void
-swm_handle_configure_notify(struct xcb_connection_t*             xcb_connection,
-                            struct xcb_configure_notify_event_t* event)
+void swm_handle_configure_notify(xcb_connection_t*    xcb_connection,
+                                 xcb_generic_event_t* event)
 {
-	enum focus_mode mode = event->window != xcb_focus_window
-	                           ? FOCUS_MODE_INACTIVE
-	                           : FOCUS_MODE_ACTIVE;
-	swm_focus_window(xcb_connection, event->window, mode);
+	auto const ev   = reinterpret_cast<xcb_configure_notify_event_t*>(event);
+	auto const mode = ev->window != xcb_focus_window ? FOCUS_MODE_INACTIVE
+	                                           : FOCUS_MODE_ACTIVE;
+	swm_focus_window(xcb_connection, ev->window, mode);
 }
 
-static void swm_handle_events(struct xcb_connection_t* xcb_connection)
+void swm_handle_events(xcb_connection_t* xcb_connection)
 {
-	xcb_generic_event_t* event = NULL;
-
-	while (1)
+	while (auto event = xcb_wait_for_event(xcb_connection))
 	{
-		event = xcb_wait_for_event(xcb_connection);
-
 		switch (event->response_type & ~0x80)
 		{
 		case XCB_CREATE_NOTIFY:
-			swm_handle_create_notify(xcb_connection,
-			                         (struct xcb_create_notify_event_t*)event);
+			swm_handle_create_notify(xcb_connection, event);
 			break;
 		case XCB_DESTROY_NOTIFY:
-			swm_handle_destroy_notify(xcb_connection,
-			                          (struct xcb_destroy_notify_event_t*)
-			                              event);
+			swm_handle_destroy_notify(xcb_connection, event);
 			break;
 		case XCB_MAP_NOTIFY:
-			swm_handle_map_notify(xcb_connection,
-			                      (struct xcb_map_notify_event_t*)event);
+			swm_handle_map_notify(xcb_connection, event);
 			break;
 		case XCB_CONFIGURE_NOTIFY:
-			swm_handle_configure_notify(xcb_connection,
-			                            (struct xcb_configure_notify_event_t*)
-			                                event);
+			swm_handle_configure_notify(xcb_connection, event);
 			break;
 		}
 
@@ -150,13 +155,14 @@ static void swm_handle_events(struct xcb_connection_t* xcb_connection)
 		free(event);
 	}
 }
+}
 
 int main(int argc, char* argv[])
 {
 	(void)argc;
 	(void)argv;
 
-	struct xcb_connection_t* xcb_connection = swm_init();
+	auto xcb_connection = swm_init();
 	swm_handle_events(xcb_connection);
 	swm_shutdown(xcb_connection);
 
